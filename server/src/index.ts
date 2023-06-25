33dc946eb7634f04353967ff19f7e98cb94d73a9
AndriiVeremchuk20 <andrejveremcuk712@gmail.com>
2023-06-25 22:59:24 +0300

    init empty project

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from "express"
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
