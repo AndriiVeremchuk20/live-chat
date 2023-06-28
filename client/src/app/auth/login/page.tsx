f7342eb59ae2024ae8bbf05d7024a61f2db6f0e8
AndriiVeremchuk20 <andrejveremcuk712@gmail.com>
2023-06-28 21:18:17 +0300

    add login page

---
+++ client/src/app/auth/login/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+const Login = () => {
+  return (
+    <div className="h-screen flex flex-row items-center justify-end bg-[url(/auth-bg.jpg)] bg-cover ">
+      <div className="w-1/2 h-screen flex flex-col justify-center items-center bg-violet-400 bg-opacity-20">
+        <form
+          className="flex flex-col w-[400px] gap-3 px-2 text-xl bg-neutral-100 rounded-lg border-violet-300 border-[2px] shadow-md"
+          autoComplete="off"
+        >
+          <span className="text-2xl font-bold text-center py-2 px-1 border-b-2 border-violet-300">
+            Sign in
+          </span>
+
+          <div className="flex flex-col mb-2">
+            <label htmlFor="email" className="text-lg">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="first_name"
+              placeholder="example@mail.com"
+              className="w-full px-2 py-1 text-xl rounded-lg border border-neutral-300 focus:outline-none focus:ring focus:border-blue-500 focus:shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col mb-2">
+            <label htmlFor="password" className="text-lg">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="passowrd"
+              placeholder="Your strong password"
+              className="w-full px-2 py-1 text-xl rounded-lg border border-neutral-300 focus:outline-none focus:ring focus:border-blue-500 focus:shadow-lg"
+            />
+          </div>
+          <button
+            className=" mb-2 text-xl text-white rounded-lg border border-neutral-300 font-bold py-1 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+            type="submit"
+          >
+            Create account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
