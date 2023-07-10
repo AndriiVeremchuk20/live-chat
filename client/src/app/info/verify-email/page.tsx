b117d74d0509aefc708e634310e25bcbcdd00ee8
AndriiVeremchuk20 <andrejveremcuk712@gmail.com>
2023-07-10 19:22:56 +0300

    update verify-email page

--- client/src/app/info/verify-email/page.tsx
+++ client/src/app/info/verify-email/page.tsx
@@ -1,7 +1,15 @@
-"use client"
+"use client";
+
+import Alert from "@/components/Alert";
 
 const VerifyEmail = () => {
-	return <div>Please verify Your email</div>
-}
+  return (
+    <div className="w-full mt-10 flex justify-center">
+      <div className="w-fit">
+        <Alert type="info" message="Please verify Your email." />
+      </div>
+    </div>
+  );
+};
 
 export default VerifyEmail;
