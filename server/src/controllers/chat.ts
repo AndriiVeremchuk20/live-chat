ab2ae57327648410f4e3d1d7a87a5b67b2a31db5
AndriiVeremchuk20 <andrejveremcuk712@gmail.com>
2023-08-01 14:40:37 +0300

    add getChatMessages controller

--- server/src/controllers/chat.ts
+++ server/src/controllers/chat.ts
@@ -1,20 +1,40 @@
 import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
 import prisma from "../../prisma";
 
-const getChatMessages = async (req: Request, res: Response, next: NextFunction) => {
+const getChatMessages = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { user } = req;
-  const {receiverId} = req.params;
+  const { receiverId } = req.params;
   if (!user) {
     return next("User not auth");
   }
 
   const messages = await prisma.message.findMany({
-	where: {
-		OR:[
-		{sender_id: user.uid, receiver_id: receiverId},
-		{receiver_id: user.uid, sender_id: receiverId}
-	]}
-  })
+    where: {
+      OR: [
+        { sender_id: user.uid, receiver_id: receiverId },
+        { receiver_id: user.uid, sender_id: receiverId },
+      ],
+    },
+    orderBy: [
+      {
+        created_at: "asc",
+      },
+    ],
+    take: 10,
+  });
+
+  res
+    .status(StatusCodes.OK)
+    .send({
+      status: "success",
+      message: "massages found",
+      data: [...messages],
+    });
 };
 
 export default { getChatMessages };
