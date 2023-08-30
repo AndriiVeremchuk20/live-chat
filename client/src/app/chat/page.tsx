059e0b03b4c9a2a40f80d5d7f96688a71d37181b
AndriiVeremchuk20 <andrejveremcuk712@gmail.com>
2023-08-30 13:01:14 +0300

    style: changed the width of the message list

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -3,7 +3,7 @@ import ChatList from "@/components/Chat/ChatList";
 const ChatPage = () => {
   return (
     <div className="flex h-screen justify-center">
-      <div className=" phone:w-full tablet:w-2/3 desktop:w-3/4">
+      <div className="phone:w-full tablet:w-2/3 desktop:w-2/4">
         <ChatList />
       </div>
     </div>
